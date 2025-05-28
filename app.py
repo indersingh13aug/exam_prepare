c76a6f5c838eefa3d76ffadd3686c9e00be9a825
Inder <kumar.inder.com>
2025-05-28 23:28:51 +0530

    update

--- app.py
+++ app.py
@@ -205,6 +205,6 @@ def generate_questions_from_string(content,complexity, num_questions=5):
 if __name__ == '__main__':
     init_db()
     # Dev
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
     # Prod
-    # app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
